e3fea41bc69c02ecbec0c4a76a75169fec99f945
root <root@flocker-1.c.oceanic-cache-718.internal>
2014-12-05 13:14:59 +0000

    finished off put runtime e/p with flocker call
    still missing:
    - DELETE runtime endpoint
    - PUT runtime fail if image not pulled on every node
    - bootstrap ssh from where armada-rest is running to every node
    - package in a Dockerfile

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ $ pip install requests
 $ pip install Flask
 $ pip install docker-py
 $ pip install PyYAML
+$ pip install port-for
+$ pip install --quiet https://storage.googleapis.com/archive.clusterhq.com/downloads/flocker/Flocker-0.3.2-py2-none-any.whl
 ```
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import pdb
 import json
 import yaml
 import docker
+import port_for
 import requests
+import subprocess
 import ConfigParser as cp
 
 from flask import Flask, request
@@ -14,46 +16,62 @@ app = Flask(__name__)
 config = cp.ConfigParser()
 config.readfp(open('cluster.properties','r'))
 
+port_store = port_for.store.PortStore()
+
+is_flocker_runtime = lambda name : name.startswith('flocker--')
+get_app_name_from_runtime_name = lambda name : name.replace('flocker--', '')
+
+def unbind_all_ports():
+  [port_store.unbind_port(bound_port_mapping[0]) for bound_port_mapping in port_store.bound_ports()]
+
 def get_cluster_name():
   return config.get('default','name')
 
-@app.route('/')
-def hello_world():
-  return 'Hello World!'
-
-is_flocker_container = lambda name : name.startswith('flocker--')
-get_app_name_from_runtime_name = lambda name : name.replace('flocker--', '')
-
-@app.route('/docker/runtimes')
+@app.route('/flocker/runtimes', methods = ['GET'])
 def get_runtimes(flocker_only=False):
   flocker_only = request.args.get('flocker_only', flocker_only)
-  state = requests.get('http://etcd.%s/v2/keys/?recursive=true' % get_cluster_name()).json()
-  nested = state['node']['nodes'][0]
-  print 'Number of cluster nodes:', len(nested['nodes'])
-  cluster_nodes = nested['nodes']
-  containers = []
-  for i, node in enumerate(cluster_nodes):
-    node_containers = [eval(n['value']) for n in node['nodes']]
-    print 'Number of containers on node %d: %d' % (i, len(node_containers))
-    containers.extend(node_containers)
+  node_states = json.loads(get_state())
+  node_states = node_states['state']
+  runtimes = []
+  for i, node in enumerate(node_states):
+    node_runtimes = [eval(n['value']) for n in node['nodes']]
+    print 'Number of containers on node %d: %d' % (i, len(node_runtimes))
+    runtimes.extend(node_runtimes)
   if flocker_only:
-    containers = [c for c in containers if is_flocker_container(c['name'])]
-  runtimes = { 'runtimes' : containers }
-  return str(json.dumps(runtimes,indent=4))
+    runtimes = [c for c in runtimes if is_flocker_runtime(c['name'])]
+  runtimes = { 'runtimes' : runtimes }
+  return json.dumps(runtimes, indent=4)
 
-@app.route('/docker/image/<path:image>', methods=['PUT'])
+@app.route('/flocker/nodes', methods = ['GET'])
+def get_nodes():
+  node_states = json.loads(get_state())
+  node_states = node_states['state']
+  print 'Number of cluster nodes:', len(node_states)
+  nodes = [node_state['key'].split('/')[-1] for node_state in node_states]
+  nodes = { 'nodes' : nodes }
+  return json.dumps(nodes, indent=4)
+
+@app.route('/flocker/state')
+def get_state():
+  state = requests.get('http://etcd.%s/v2/keys/?recursive=true' % get_cluster_name()).json()
+  node_states = state['node']['nodes'][0]['nodes']
+  node_states = { 'state' : node_states }
+  return json.dumps(node_states, indent=4)
+
+@app.route('/flocker/image/<path:image>', methods = ['PUT'])
 def put_image(image):
   print image, request.args
   if request.args.has_key('tag'):
     image = image + ':' + request.args.get('tag')
-  nodes = ['10.240.202.202', '10.240.112.213']
+  nodes = json.loads(get_nodes())
+  nodes = nodes['nodes']
   for node in nodes:
     cli = docker.Client(base_url='tcp://%s:4243' % node)
     for line in cli.pull(image, stream=True):
       print(json.dumps(json.loads(line), indent=4))
   return 'True'
 
-@app.route('/docker/runtime', methods=['PUT'])
+@app.route('/flocker/runtime', methods=['PUT'])
 def put_runtime():
   uploaded_files = request.files.values()
   assert len(uploaded_files) > 0
@@ -93,6 +111,24 @@ def put_runtime():
 
   assert new_runtime_name not in app_config.keys()
 
+  # change external ports to a random port
+  current_external_ports = []
+  for runtime in runtimes:
+    current_external_ports.extend([int(port['external']) for port in runtime['ports'] if port['external'] != None])
+  available_external_ports = port_for.available_good_ports().difference(set(current_external_ports))
+  
+  new_runtime_internal_ports = [port[max(port.find(':') + 1, 0):] for port in new_runtime_config[new_runtime_name]['ports']]
+  new_runtime_internal_ports = [int(port) for port in new_runtime_internal_ports]
+  new_port_mappings = []
+
+  unbind_all_ports()
+  for internal_port in new_runtime_internal_ports:
+    bound_port_name = new_runtime_name + '-' + str(internal_port)
+    external_port = port_store.bind_port(bound_port_name)
+    new_port_mappings.append('%d:%d' % (external_port, internal_port))
+  unbind_all_ports()
+
+  new_runtime_config[new_runtime_name]['ports'] = new_port_mappings
   app_config.update(new_runtime_config)
   
   print app_config
@@ -120,7 +156,14 @@ def put_runtime():
   dep_yml.write(yaml.dump(dep_config))
   dep_yml.close()
 
+  proc = subprocess.Popen('flocker-deploy deployment.yml application.yml', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  print proc.communicate()
+
   return 'True'
+
+@app.route('/flocker/runtime', methods = ['DELETE'])
+def delete_runtime():
+  pass
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
