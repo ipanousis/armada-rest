b76e483d6a370c3501c166fa7b150d82a44a4a6e
root <root@flocker-1.c.oceanic-cache-718.internal>
2014-12-08 02:00:28 +0000

    fix ssh-copy-id script

--- bin/upload_ssh_keys.sh
+++ bin/upload_ssh_keys.sh
@@ -11,6 +11,6 @@ fi
 NODES=`curl localhost:5000/flocker/state/nodes | egrep -o "([0-9]+\.){3}[0-9]+"`
 
 for NODE in $NODES; do
-  sshpass -p kattlefish ssh-copy-id -i /root/.ssh/id_rsa root@$NODE
+  sshpass -p kattlefish ssh-copy-id -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa root@$NODE
 done
 
